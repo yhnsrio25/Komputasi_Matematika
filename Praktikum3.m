a860c3424276cbd0e0dd59d1a8390e6b9cec6cfc
yhnsrio25 <yohanesriodewantoro@gmail.com>
2025-04-14 00:23:43 -0700

    Praktikum 3

---
+++ Praktikum3.m
@@ -0,0 +1,78 @@
+clc
+clear
+
+%if... end
+% n = input('nilai ; ');
+% if n > 70
+% 	disp('anda lulus');
+% end
+% disp('coba lagi');
+
+% if... else... end
+% n = input('nilai ; ');
+% if n > 70
+% 	disp('anda lulus');
+% else
+% 	disp('coba lagi');
+% end
+
+%if...elseif..elseif..end
+% n = input('tinggi badan: ');
+% if n >= 190
+%     disp('sangat tinggi');
+% elseif n > 160 && n < 190
+%     disp('cukup tinggi');
+% elseif n <= 160 
+%     disp('pendek');
+% else
+%     disp('mini');
+% end
+
+% switch
+% hitungan = input('operasi : ');
+% a = 2
+% b = 3
+% switch hitungan
+% 	case 1
+% 		y = a+b
+% 	case 2
+% 		y = a-b
+% 	case 3
+% 		y = a*b
+% 	otherwise
+% 		disp('bukan pilihan');
+% end
+
+% loop (pengulangan)
+%for_1
+% for rio = 1:10S
+% 	disp(rio);
+% end
+
+% for_2
+% a = 0;
+% for rio = 1:5
+% 	a = a+rio;
+% end
+% disp(a)
+
+%continue
+% a = 0;
+% for rio = 1:5
+%     if rio == 2
+%         continue
+%     else
+%         a = a+rio;
+%     end
+% end
+% disp(a)
+
+%break
+% a = 0;
+% for rio = 1:5
+%     if rio == 5
+%         break
+%     else
+%         disp(rio);
+%     end
+% end
